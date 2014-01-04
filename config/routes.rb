2861aebef346126c9d80f9ec28dbf92cf2e07c7d
tj8295 <tch399@gmail.com>
2014-01-04 12:58:34 -0500

    fixed post view

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,20 @@
 Blog::Application.routes.draw do
   get "welcome/index"
+
+  resources :posts do
+    resources :comments
+  end
+
+  root 'welcome#index'
+
+end
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  resources :posts
 
-  root 'welcome#index'
+
 
 
 
@@ -58,4 +66,3 @@ Blog::Application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
-end
