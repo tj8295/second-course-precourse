8efa096c6b5d0cbb795a4a044d1a0173c5577e2b
tj8295 <tch399@gmail.com>
2014-01-05 21:31:43 -0500

    added relationship

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140104012135) do
+ActiveRecord::Schema.define(version: 20140104173131) do
 
   create_table "comments", force: true do |t|
     t.string   "commenter"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 20140104012135) do
     t.text     "text"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "user_id"
   end
 
 end
